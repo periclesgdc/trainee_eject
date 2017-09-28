392eb7236c56ec7b89a0b0b190099657e9fec779
periclesgdc <periclesgdc@gmail.com>
2017-09-28 17:07:44 -0300

    Atualização no simplemooc

--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^$', 'simplemooc.core.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', include('simplemooc.core.urls', namespace='core'), name='home'),
 ]
